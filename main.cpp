8d06c3abf494f0088031c80fcf4032b6d1ffc83a
Leonardo Silva Dutra <leo.dutra012@gmail.com>
2020-04-29 23:13:25 -0300

    Implemented dialogue in main function.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
-
+#include "ConsoleDialogue.h"
 
 
 int main(int argc, char *argv[])
 {
+    ConsoleDialogue dialogue(nullptr);
 
+
+    LOAD_STATUS status = dialogue.loadDialogue("D:/MyDialogue.xml");
+    
+    if (status == INVALID_FILE)
+        out << "Could not open file.\n\n" << flush;
+
+    else if (status == INVALID_CONTENT)
+        out << "Could not set content.\n\n" << flush;
+
+    else if (status == NO_NODE_ID)
+        out << "Node with missing ID found.\n\n" << flush;
+
+    else if (status == NO_NODE_TEXT)
+        out << "Node with missing text found.\n\n" << flush;
+
+    else if (status == NO_OPTION_TEXT)
+        out << "Option with missing text found.\n\n" << flush;
+
+    else if (status == NO_OPTION_DESTINATION_ID)
+        out << "Option with missing destination ID found.\n\n" << flush;
+
+    else
+        dialogue.run();
+
+    out << "\n\t---END OF DIALOGUE---\n\n" << flush;
+    out << "Press ENTER to exit..." << flush;
+    in.read(1);
+
+    return 0;
 }
